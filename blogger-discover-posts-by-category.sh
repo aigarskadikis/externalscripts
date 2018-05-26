cdc0285aa912ac8a414b309b4e242f5ba269caa8
catonrug <aigars.kadikis@gmail.com>
2018-05-26 23:30:06 +0000

    next

--- blogger-discover-posts-by-category.sh
+++ blogger-discover-posts-by-category.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -s "https://$1/feeds/posts/default/-/$2/?atom.xml?redirect=false&start-index=1&max-results=50" | egrep -o "\/posts\/default\/[0-9]+" |sort|uniq| egrep -o "[0-9]+" | sed "s/^/{\"{#POST}\":\"/;s/$/\"},/" | tr -cd "[:print:]" | sed "s/^/{\"data\":[/;s/,$/]}/"
+curl -s "https://$1/feeds/posts/default/-/$(echo "$2"|sed "s/ /%20/g")/?atom.xml?redirect=false&start-index=1&max-results=50" | egrep -o "\/posts\/default\/[0-9]+" |sort|uniq| egrep -o "[0-9]+" | sed "s/^/{\"{#POST}\":\"/;s/$/\"},/" | tr -cd "[:print:]" | sed "s/^/{\"data\":[/;s/,$/]}/"
 
