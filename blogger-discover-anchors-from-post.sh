b116f9cb4b57a40b9d5098c092dc5e08dc657d84
catonrug <aigars.kadikis@gmail.com>
2018-06-01 22:55:21 +0300

    next

--- blogger-discover-anchors-from-post.sh
+++ blogger-discover-anchors-from-post.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-curl -s "https://$1/feeds/posts/default/$2?alt=json" | jq -r '.entry|.content|."$t"' | grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//' -e 's/["'"'"']$//' | sed "s/^\//https:\/\/$1\//" | sed "s/^/{\"{#LINK}\":\"/;s/$/\"},/" | tr -cd "[:print:]" | sed "s/^/{\"data\":[/;s/,$/]}/"
+list=$(curl -s "https://$1/feeds/posts/default/$2?alt=json" | \
+jq -r '.entry|.content|."$t"' | \
+grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | \
+sed -e 's/^<a href=["'"'"']//' -e 's/["'"'"']$//' | \
+sed "s/^\//https:\/\/$1\//")
 
+echo "$list" | wc -l
+
+zabbix_sender --zabbix-server 127.0.0.1 \
+	--host "$1" \
+	-k "discover.anchors" \
+	-o $(echo "$list" | \
+	sed "s/^/{\"{#LINK}\":\"/;s/$/\",\"{#ID}\":\"$2\"},/" | \
+	tr -cd "[:print:]" | \
+	sed "s/^/{\"data\":[/;s/,$/]}/")
 
