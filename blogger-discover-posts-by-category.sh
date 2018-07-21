9b7b87b0692b1ffa87f9f0ccf089bab1f9736702
catonrug <aigars.kadikis@gmail.com>
2018-07-21 09:25:19 +0300

    next

--- blogger-discover-posts-by-category.sh
+++ blogger-discover-posts-by-category.sh
@@ -1,44 +1,21 @@
 #!/bin/bash
 
-#maximum count blogger can contain per one sitemap view is 50 posts
-#that is why we need to create array to put all post ids inside 
-declare -a array
-
-nr=-49 #start check from page 0
-count=1 #loop will continue while there is at least one post in sitemap
-
-#loop starts because we definet so in previous step
-while [ "$count" -gt "0" ]
-do
-
-#how many posts should check per one view
-nr=$((nr+50))
-
-#set up full url link
-url="https://$1/feeds/posts/default/-/$(echo "$2"|sed "s/ /%20/g")/?atom.xml?redirect=false&start-index=$nr&max-results=50"
-
-#uncoment for debuging
-#echo "$url"
-
-#check if url exist
-httpcode=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-
-if [ "$httpcode" -eq "200" ]; then
-array[nr]=$(curl -s "$url" | sed "s/<entry>/\n<entry>/g" |grep "^<entry>"| sed "s/<entry>.*blog-[0-9]\+\.post-/{\"{#POST}\":\"/g" | sed "s/<\/id>.*<title.type=.text.>/\",\"{#TITLE}\":\"/g" | sed "s/<\/title>.*$/\"},/g")
-
-#count how many posts is there
-count=$(echo "${array[nr]}"|grep -v "^$"|wc -l)
-
-#uncomment for debuging
-#echo "${array[nr]}"
-#echo $count
-#echo
-
-fi
-
-done
-
-#output all array elements
-#convert output to JSON format for Zabbix LLD dicover prototype
-echo "${array[@]}" | sort | uniq | tr -cd "[:print:]" | sed "s/[ \t]*$//" | sed "s/^/{\"data\":[/;s/,$/]}/"
+curl -s "https://$1/feeds/posts/default/-/$(echo "$2"|sed "s/ /%20/g")?alt=json-in-script&max-results=100" |\
+sed "s/gdata.io.handleScriptLoaded(//;s/);$//" |\
+jq '.["feed"] | .["entry"] | .[] | (.link[] | select(.rel == "alternate").href),(.link[] | select(.rel == "alternate").title),.id."$t"' |\
+sed "s/tag:blogger.com,1999:blog-.*.post-//g" |\
+sed ': loop;
+i {\"{#URL}\":
+a ,
+n;
+i \"{#TITLE}\":
+a ,
+n;
+i \"{#POST}\":
+a },
+n;
+b loop' |\
+tr -cd "[:print:]" |\
+sed "s/[ \t]*$//" |\
+sed "s/^/{\"data\":[/;s/,$/]}/"
 
