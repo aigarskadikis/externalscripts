656f49c3d425f4183203c4769b3eac087574ce0e
catonrug <aigars.kadikis@gmail.com>
2018-07-25 09:29:53 +0300

    next

--- retrieve-all-host-ip-addreses-from-hostid.py
+++ retrieve-all-host-ip-addreses-from-hostid.py
@@ -1,11 +1,4 @@
 #!/bin/python
-"""
-Shows a list of all current issues (AKA tripped triggers)
-"""
-
-import os
-import json
-import string
 from pyzabbix import ZabbixAPI
 
 #import credentials from external file
@@ -15,12 +8,11 @@ import config
 
 # The hostname at which the Zabbix web interface is available
 ZABBIX_SERVER = config.url
-
 zapi = ZabbixAPI(ZABBIX_SERVER)
 
 # Login to the Zabbix API
 zapi.login(config.username, config.password)
 
-for host in zapi.item.get(output='extend',search={'key_':'zabbix[proxy,Home,lastaccess]'},sortfield='name'):
+for host in zapi.item.get(output='extend',search={'key_':'icmppingloss[,100,40,1500,400]'}):
   for host_conn in zapi.hostinterface.get(output='extend',hostids=host['hostid']):
     print host_conn['ip']
