f0c763a01b21e062efaf6522390fa1b02fc259a7
catonrug <aigars.kadikis@gmail.com>
2018-07-31 12:18:51 +0300

    next

--- list-all-items-per-every-host.py
+++ list-all-items-per-every-host.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys
+from pprint import pprint
 from pyzabbix import ZabbixAPI
 sys.path.insert(0,'/var/lib/zabbix')
 import config
@@ -7,4 +8,4 @@ ZABBIX_SERVER = config.url
 zapi = ZabbixAPI(ZABBIX_SERVER)
 zapi.login(config.username, config.password)
 result = zapi.host.get(selectItems = ['itemid', 'name', 'key_'], selectTriggers = ['triggerid', 'description', 'expression'])
-print(result)
+pprint(result)
