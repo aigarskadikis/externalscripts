c556f0fc24ecededa741cd19b0c368d4da917611
catonrug <aigars.kadikis@gmail.com>
2018-08-01 16:59:49 +0300

    next

--- show-empty-hostgroups-with-exceptions.sh
+++ show-empty-hostgroups-with-exceptions.sh
@@ -14,18 +14,20 @@ file.close()
 
 # use Zabbix API procedure hostgroup.get to get all hostgroups 
 # +execute selectHosts query to get array of assigned hosts
-for hosts in zapi.hostgroup.get(output='extend',selectHosts='query'):
+for hosts in zapi.hostgroup.get(output='extend',selectHosts='query',selectTemplates='query'):
   # detects if array is empty
   if not hosts['hosts']:
-    # check if item is in list
-    if hosts['name'] not in exception_list:
-      # print the hostgroup name
-      print hosts
-      if len(sys.argv) > 1:
-        if str(sys.argv[1]) == 'delete':
-          print 'deleting ' + hosts['groupid']
-          try:
-            zapi.hostgroup.delete(hosts['groupid'])
-          except ZabbixAPIException, e:
-            print e
+    # check if there are no templates assigned to this host group
+    if not hosts['templates']:
+      # check if item is not in exception list
+      if hosts['name'] not in exception_list:
+        # print the hostgroup name
+        print hosts
+        if len(sys.argv) > 1:
+          if str(sys.argv[1]) == 'delete':
+            print 'deleting ' + hosts['groupid']
+            try:
+              zapi.hostgroup.delete(hosts['groupid'])
+            except ZabbixAPIException, e:
+              print e
           
