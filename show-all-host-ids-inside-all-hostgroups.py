066ed1b51cd22b7182b58046c0445b72c7241da4
catonrug <aigars.kadikis@gmail.com>
2018-08-01 10:44:17 +0300

    next

--- show-all-host-ids-inside-all-hostgroups.py
+++ show-all-host-ids-inside-all-hostgroups.py
@@ -1,23 +1,16 @@
 #!/bin/python
-from pprint import pprint
 from pyzabbix import ZabbixAPI
-
-#import credentials from external file
 import sys
 sys.path.insert(0,'/var/lib/zabbix')
 import config
-
-# The hostname at which the Zabbix web interface is available
 ZABBIX_SERVER = config.url
-
 zapi = ZabbixAPI(ZABBIX_SERVER)
-
-# Login to the Zabbix API
 zapi.login(config.username, config.password)
 
-# Get a list of all issues (AKA tripped triggers)
+# use Zabbix API procedure hostgroup.get to get all hostgroups 
+# +execute selectHosts query to get array of assigned hosts
 for hosts in zapi.hostgroup.get(output='extend',selectHosts='query'):
   # detects if array is empty
   if not hosts['hosts']:
+    # show the group name
     print hosts['name']
-
