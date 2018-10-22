c03401bd1e017587fce6c0bf5f23cd42aff767f5
catonrug <aigars.kadikis@gmail.com>
2018-10-22 12:31:56 +0300

    next

--- show-latest-values.py
+++ show-latest-values.py
@@ -4,6 +4,7 @@ from pyzabbix import ZabbixAPI, ZabbixAPIException
 
 # import credentials from external file
 import sys
+from pprint import pprint
 sys.path.insert(0, '/var/lib/zabbix')
 import config
 # we will search latest very latest values
@@ -19,19 +20,19 @@ zapi = ZabbixAPI(ZABBIX_SERVER)
 # Login to the Zabbix API
 zapi.login(config.username, config.password)
 
-# latest 'Free swap space' values on 'Zabbix server' in laste 10 minutes
+# latest 'Free swap space' values on 'Zabbix server' in laste 5 minutes
 print
 print "Current unixtime:"
 print ctime
 print
 
 print "Execute only filter and use itemid:"
-var = zapi.history.get(filter={"itemid":"23309"},history=3, time_from = ctime-600, time_till = ctime)
-print var
+var = zapi.history.get(filter={"itemid":"23309"},history=3, time_from = ctime-300, time_till = ctime)
+pprint(var)
 print
 
 print "Use parameters: 'hostids' and 'itemids':"
-var = zapi.history.get(hostids=10084,itemids=23309,history=3, time_from = ctime-600, time_till = ctime)
-print var
+var = zapi.history.get(hostids=10084,itemids=23309,history=3, time_from = ctime-300, time_till = ctime)
+pprint(var)
 print
 
