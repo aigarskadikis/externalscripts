67329eddde7dbac5d175e6c57239df59f46f9b44
catonrug <aigars.kadikis@gmail.com>
2018-11-07 06:33:50 +0200

    next

--- create-json-object-one-variable-flexible.sh
+++ create-json-object-one-variable-flexible.sh
@@ -3,7 +3,7 @@ IFS=':, ' read -r -a array <<< "$1"
 idx=0
 echo {\"data\":[
 while [ -n "${array[$idx]}" ]; do
-echo -n \{\"{#path}\":\""${array[$idx]}"\}
+echo -n \{\"{#PATH}\":\""${array[$idx]}"\}
 let idx=$idx+1
 [ -n "${array[idx]}" ] && echo "," || echo
 done
