ecf0718e4fda24c4217fa095fb7900487e3e93b9
catonrug <aigars.kadikis@gmail.com>
2018-12-14 14:43:52 +0200

    next

---
+++ host-group-list-all-hosts.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+"""
+Shows a list of all current issues (AKA tripped triggers)
+"""
+
+#for argument support
+import sys
+from pprint import pprint
+
+#pip install pyzabbix
+from pyzabbix import ZabbixAPI
+
+#import api credentials from different file
+sys.path.insert(0,'/var/lib/zabbix')
+import config
+
+# The hostname at which the Zabbix web interface is available
+ZABBIX_SERVER = config.url
+
+zapi = ZabbixAPI(ZABBIX_SERVER)
+
+# Login to the Zabbix API
+zapi.login(config.username, config.password)
+
+request=zapi.do_request('hostgroup.get', {"selectHosts":"extend", "output": "extend", "filter": { "name": [ "Zabbix servers", "Linux servers" ] } })
+
+pprint(request)
