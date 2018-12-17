fae68ab8043c910397a75ff5e373810bc8917182
catonrug <aigars.kadikis@gmail.com>
2018-12-17 10:33:36 +0200

    next

--- host-group-list-all-hosts.py
+++ host-group-list-all-hosts.py
@@ -5,24 +5,21 @@ import sys
 from pprint import pprint
 from pyzabbix import ZabbixAPI
 
-# authorize in API
 sys.path.insert(0,'/var/lib/zabbix')
 import config
 ZABBIX_SERVER = config.url
 zapi = ZabbixAPI(ZABBIX_SERVER)
 zapi.login(config.username, config.password)
 
-# main program
 hostgroup = sys.argv[1]
-request=zapi.do_request('hostgroup.get',{"selectHosts":"extend","output": "extend","filter": { "name": [ hostgroup ] } })
+request=zapi.hostgroup.get(output='extend',selectHosts='extend',filter={'name': hostgroup})
 
-for r in request["result"]:
- for host in r["hosts"]:
-  print "Checking "+host["name"] + ' (hostid:' + host["hostid"]+") for template \"" +sys.argv[2] + "\""
-
-  for t in zapi.host.get(selectParentTemplates=["templateid","name"],output='extend',hostids=host["hostid"]):
+for host in request:
+ for h in host["hosts"]:
+  print "Checking "+h["name"] + ' (hostid:' + h["hostid"]+") for template \"" +sys.argv[2] + "\""
+  for t in zapi.host.get(selectParentTemplates=["templateid","name"],output='extend',hostids=h["hostid"]):
    for e in t["parentTemplates"]:
     if (str(e["name"]) == str(sys.argv[2])):
-     print "TEMPLATE \""+ e["name"] +"\" (templateid:"+e["templateid"]+") will get unlinked from host \""+host["name"] +"\" (hostid:" + host["hostid"]+ ")\n"
-     zapi.host.update(hostid=host["hostid"],templates_clear={'templateid':e["templateid"]})
+     print "TEMPLATE \""+ e["name"] +"\" (templateid:"+e["templateid"]+") will get unlinked from host \""+h["name"] +"\" (hostid:" + h["hostid"]+ ")\n"
+     zapi.host.update(hostid=h["hostid"],templates_clear={'templateid':e["templateid"]})
 
