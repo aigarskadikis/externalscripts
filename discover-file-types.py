f64c6eea27818a9aea8acc346166ce75dac563ad
catonrug <aigars.kadikis@gmail.com>
2018-12-21 12:28:56 +0200

    next

--- discover-file-types.py
+++ discover-file-types.py
@@ -1,20 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import os
 import sys
 import json
 
-extension = sys.argv[1]
-logdir = sys.argv[2]
-
-
+logdir = sys.argv[1]
+extensions = sys.argv[2:]
 data = []
 
 for (logdir, _, files) in os.walk(logdir):
-    for f in files:
-        if f.endswith(extension):
-            path = os.path.join(logdir, f)
-            data.append({'{#FULLFILEPATH}':path})
-            jsondata = json.dumps(data)
+ for f in files:
+  for extension in extensions:
+   if f.endswith(extension):
+    path = os.path.join(logdir,f)
+    data.append({'{#FULLFILEPATH}':path})
+    jsondata = json.dumps(data)
 
 print json.dumps({"data": data})
 
