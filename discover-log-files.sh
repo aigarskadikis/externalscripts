4c2564283eeec6bb151696033608de6682f7e18c
catonrug <aigars.kadikis@gmail.com>
2019-01-02 12:30:13 +0200

    next

--- discover-log-files.sh
+++ discover-log-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CALL=$(find /var/log/zabbix/`date +%m`-`date +%Y`/`date +%d`/ -name zabbix_server.log)
+CALL=$(find /var/log/zabbix -name zabbix_server.*.gz)
+#CALL=$(find /var/log/zabbix/`date +%m`-`date +%Y`/`date +%d`/ -name zabbix_server.log)
 MACRO=LOGFILE
-#echo $CALL | xargs -n1  | cut -d'>' -f2- | cut -d'<' -f1
 COUNT=$(echo $CALL| xargs -n1 | wc -l)
 printf "{\"data\":[\n"
 echo $CALL | xargs -n1 | cut -d'>' -f2- | cut -d'<' -f1 | while read line; do \
