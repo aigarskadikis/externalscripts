61e0e9b01ddf754d3580a15dec8ee8c11f4dad5d
catonrug <aigars.kadikis@gmail.com>
2019-06-26 11:11:03 +0300

    next

--- replaceIPaddressAgentInterface.py
+++ replaceIPaddressAgentInterface.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 from pyzabbix import ZabbixAPI
 import urllib3
+from pprint import pprint
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import sys
 sys.path.insert(0,'/var/lib/zabbix')
@@ -11,4 +12,6 @@ zapi.session.verify=False
 
 zapi.login(config.username, config.password)
 
-print zapi.host.get ({"output":"hostid","filter":{"host":"Zabbix server"}})[0]['hostid']
+hostid=zapi.host.get({"output":"hostid","filter":{"host":"Zabbix server"}})[0]['hostid']
+
+pprint(zapi.hostinterface.get(output=["dns","ip","useip"],selectHosts=["hosts"],filter={"main": 1, "type": 1},hostids=["10084"]))
