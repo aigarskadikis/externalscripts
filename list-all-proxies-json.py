552037b62ebe644c947c8431701e4350d7b03dc6
catonrug <aigars.kadikis@gmail.com>
2019-07-11 12:48:10 +0300

    next

--- list-all-proxies-json.py
+++ list-all-proxies-json.py
@@ -1,10 +1,13 @@
 #!/bin/env python
 from pyzabbix import ZabbixAPI
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import sys
 sys.path.insert(0,'/var/lib/zabbix')
 import config
 ZABBIX_SERVER = config.url
 zapi = ZabbixAPI(ZABBIX_SERVER)
+zapi.session.verify=False
 zapi.login(config.username, config.password)
 result = zapi.proxy.get()
 print "{\"data\":["
