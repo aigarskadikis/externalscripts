3b44514c4d0b51cfa0dec81d3b29efbcacac161a
catonrug <aigars.kadikis@gmail.com>
2019-11-01 12:13:38 +0200

    next

---
+++ postgres_backup_simple.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+day=$(date +%Y%m%d)
+clock=$(date +%H%M)
+dest=~/10/backups/$day/$clock
+
+if [ ! -d "$dest" ]; then
+  mkdir -p "$dest"
+fi
+
+databases2backup=$(psql -t -c "SELECT datname FROM pg_database" | awk '$1=$1' | grep "^z.*")
+
+echo "$databases2backup" | while IFS= read -r db
+do {
+echo "$db"
+pg_dump $db | gzip --best > $dest/$db.sql.gz
+} done
+
+rclone --delete-empty-src-dirs -vv move ~/10/backups ZabbixBackupPostgreSQL:ZabbixBackupPostgreSQL
+
