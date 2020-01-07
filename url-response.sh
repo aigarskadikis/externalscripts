5c58abf343773ab947c0904c56e2ee38f5116e8e
catonrug <aigars.kadikis@gmail.com>
2020-01-07 08:45:18 +0000

    next

--- url-response.sh
+++ url-response.sh
@@ -3,5 +3,5 @@ echo "$1" | grep "care.dlservice" > /dev/null
 if [ "$?" -ne "0" ]; then
 curl -o /dev/null -m 25 -s -w %{http_code} "$1"
 else
-wget --spider -S "$1" 2>&1 | -m1 grep "HTTP/" | awk '{print $2}'
+wget --spider -S "$1" 2>&1 | grep -m1 "HTTP/" | awk '{print $2}'
 fi
