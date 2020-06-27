e5bd9542aeb1d98869e7fbcdcf12e4ee68eb265d
catonrug <aigars.kadikis@gmail.com>
2020-06-27 18:51:52 +0300

    next

--- backup_zabbix_history_trends_before_yesterday.sh
+++ backup_zabbix_history_trends_before_yesterday.sh
@@ -2,7 +2,6 @@
 destination=/backup/zabbix/mysql/raw
 mkdir -p $destination
 yesterday=$(date -d "1 DAY AGO" "+%Y-%m-%d")
-today=$(date -d "0 DAY AGO" "+%Y-%m-%d")
 echo "
 history
 history_uint
@@ -16,17 +15,15 @@ grep -v "^$" | \
 while IFS= read -r table; do {
 echo $table
 old=$(echo $table|sed "s|$|_old|")
-if [ ! -f "$destination/$table.sql.xz.$yesterday" ]; then
+if [ ! -f "$destination/$old.sql.xz.before.$yesterday" ]; then
 mysqldump \
 --flush-logs \
 --single-transaction \
 --no-create-info \
 zabbix $table --where=" \
-clock >= UNIX_TIMESTAMP(\"$yesterday 00:00:00\") \
-AND \
-clock < UNIX_TIMESTAMP(\"$today 00:00:00\") \
-" | sed "s|$table|$old|" > $destination/$table.sql && \
-xz $destination/$table.sql && \
-mv $destination/$table.sql.xz $destination/$table.sql.xz.$yesterday
+clock < UNIX_TIMESTAMP(\"$yesterday 00:00:00\") \
+" | sed "s|$table|$old|" > $destination/$old.sql && \
+xz $destination/$old.sql && \
+mv $destination/$old.sql.xz $destination/$old.sql.xz.before.$yesterday
 fi
 } done
