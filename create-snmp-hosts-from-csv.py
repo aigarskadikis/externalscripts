0a5c371629655664dbda03baf1ce3f423631e49a
catonrug <aigars.kadikis@gmail.com>
2020-10-23 21:59:27 +0300

    next

--- create-snmp-hosts-from-csv.py
+++ create-snmp-hosts-from-csv.py
@@ -1,10 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # import hosts from txt file to Zabbix via API
 # and assign template and host group to them
+
+# python3 required
+# dnf install python3-pip
+# pip3 install zabbix_api
+# script is tested and works with module 'zabbix_api'
+# which is not the same as 'pyzabbix' !
 import csv
 
-# pip install pyzabbix
-#from pyzabbix import ZabbixAPI
 from zabbix_api import ZabbixAPI
 
 import urllib3
@@ -20,7 +24,7 @@ zapi = ZabbixAPI(ZABBIX_SERVER)
 zapi.session.verify=False
 zapi.login(config.username, config.password)
 
-file = open("hostlist.csv",'rb')
+file = open("hostlist.csv",'rt')
 reader = csv.DictReader( file )
 
 # take the file and read line by line
@@ -28,10 +32,10 @@ for line in reader:
  
  # check if this host exists in zabbix
  if not zapi.host.get({"filter":{"host" :line['name']}}):
-  print line['name'],"not yet registred"
+  print (line['name'],"not yet registred")
   if zapi.proxy.get({"output": "proxyid","selectInterface": "extend","filter":{"host":line['proxy']}}):
    proxy_id=zapi.proxy.get({"output": "proxyid","selectInterface": "extend","filter":{"host":line['proxy']}})[0]['proxyid']
-   if proxy_id>0:
+   if int(proxy_id)>0:
      templates=line['template'].split(";")
      groups=line['group'].split(";")
      # take first group from group array
@@ -40,7 +44,7 @@ for line in reader:
      template_id = zapi.template.get({"filter" : {"name" : templates[0]}})[0]['templateid']
      # crete a host an put hostid instantly in the 'hostid' variable
      hostid = zapi.host.create ({
-        "host":line['name'],"interfaces":[{"type":2,"dns":"","main":1,"ip": line['address'],"port": 161,"useip": 1,}],
+        "host":line['name'],"interfaces":[{"type":2,"dns":"","main":1,"ip": line['address'],"port": 161,"useip": 1,"details":{"version":3,"bulk":0,"securityname":"mysecurityname","contextname":"","securitylevel":1}}],
         "groups": [{ "groupid": group_id }],
         "proxy_hostid":proxy_id,
         "templates": [{ "templateid": template_id }]})['hostids']
@@ -56,9 +60,9 @@ for line in reader:
           try:
            nt=zapi.template.massadd({"templates":tid,"hosts":hostid})
           except Exception as nt:
-           print "template",one_template,"probably already linked"
+           print ("template",one_template,"probably already linked")
         else:
-          print "Temnplate:",one_template,"does not exist"
+          print ("Temnplate:",one_template,"does not exist")
        except Exception as tid:
         print("Temnplate:",one_template,"does not exist")
 
@@ -72,16 +76,16 @@ for line in reader:
           try:
            nhg=zapi.hostgroup.massadd({"groups":gid,"hosts":hostid})
           except Exception as nhg:
-           print "Hostgroup",one_hostgroup,"probably already linked"
+           print ("Hostgroup",one_hostgroup,"probably already linked")
         else:
-          print "Hostgroup",one_hostgroup,"does not exist"
+          print ("Hostgroup",one_hostgroup,"does not exist")
        except Exception as gid:
-        print "Hostgroup",one_hostgroup,"does not exist"
+        print ("Hostgroup",one_hostgroup,"does not exist")
 
    else:
-    print "proxy id does not exist"
+    print ("proxy id does not exist")
 
  else:
-   print line['name'],"already exist"
+   print (line['name'],"already exist")
 
 file.close()
